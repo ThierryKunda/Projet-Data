599d80eb11d311fb8cdfa68b4b70c53588a88e19
ThierryKunda <kundathierry@gmail.com>
2025-06-01 18:17:00 +0200

    Début section Votre profil

--- app/frontend/src/components/sections/UserProfile.tsx
+++ app/frontend/src/components/sections/UserProfile.tsx
@@ -1,3 +1,10 @@
 export default function UserProfile() {
-    return <div></div>
+    return <div>
+        <label for="invest-profile">Profil d'investissement</label>
+        <select id="invest-profile" class="form-select">
+            <option value="balanced">Équilibré</option>
+            <option value="beginner">Débutant</option>
+            <option value="aggressive">Agressif</option>
+        </select>
+    </div>
 }
